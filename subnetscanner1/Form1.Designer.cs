147d9bddbcf8dcf422b6f714ca36b14f53ecdb0d
Michael Lauritsen <i30288741@live.dk>
2018-07-16 08:48:53 +0200

    Add project files.

---
+++ subnetscanner1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace subnetscanner1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdScan = new System.Windows.Forms.Button();
+            this.cmdReset = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtHosts = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdScan
+            // 
+            this.cmdScan.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdScan.Location = new System.Drawing.Point(287, 12);
+            this.cmdScan.Name = "cmdScan";
+            this.cmdScan.Size = new System.Drawing.Size(75, 23);
+            this.cmdScan.TabIndex = 0;
+            this.cmdScan.Text = "Scan";
+            this.cmdScan.UseVisualStyleBackColor = true;
+            this.cmdScan.Click += new System.EventHandler(this.cmdScan_Click);
+            // 
+            // cmdReset
+            // 
+            this.cmdReset.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdReset.Location = new System.Drawing.Point(368, 12);
+            this.cmdReset.Name = "cmdReset";
+            this.cmdReset.Size = new System.Drawing.Size(75, 23);
+            this.cmdReset.TabIndex = 1;
+            this.cmdReset.Text = "Reset";
+            this.cmdReset.UseVisualStyleBackColor = true;
+            this.cmdReset.Click += new System.EventHandler(this.cmdReset_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 12);
+            this.txtIP.Multiline = true;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(113, 23);
+            this.txtIP.TabIndex = 5;
+            // 
+            // txtHosts
+            // 
+            this.txtHosts.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHosts.Location = new System.Drawing.Point(12, 55);
+            this.txtHosts.Multiline = true;
+            this.txtHosts.Name = "txtHosts";
+            this.txtHosts.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHosts.Size = new System.Drawing.Size(431, 169);
+            this.txtHosts.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "eks. 192.168.0.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 238);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHosts);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.cmdReset);
+            this.Controls.Add(this.cmdScan);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Subnet Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdScan;
+        private System.Windows.Forms.Button cmdReset;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtHosts;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ subnetscanner1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net; //bibliotek til at tilgå netværket, indeholder en masse classes som skal bruges bla. IPHostentry og dns info
+using System.Net.NetworkInformation; // bibliotek til at tilgå classes der kan bruges til ping.
+using System.Net.Sockets; //bliver i den her her program brugt til hvis der kommer svar fra en host uden svar uden navn (firewall)
+
+
+
+namespace subnetscanner1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            //Thread Control der "desværre" er brug for. siger at der ikke skal tjekkes for crossthreads
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        //counter til antal fundet host
+        static CountdownEvent countdown; //event til total antl host.
+        static int upCount = 0; // tæller til Host. starter på 0
+        static object Obj = new object(); //laver et object til tæller af host (hver gang der bliver fundet en i vores loop)
+        const bool resolveNames = true; // vi laver en konstant variabel som en Bool til at gemme alle host navne. en Bool kan kun ha 2 værdier (sandt eller falsk)
+
+        //Thread der søger på det først starter når der bliver trykket på scan
+        Thread myThread = null;
+
+        //Ping completed hvad der sker når der kommer et svar tilbage fra et ping dette bliver gjort for hvert enkelt tal fra 1-255
+        private void P_PingCompleted(object sender, PingCompletedEventArgs e) //vi laver en metode/funktion pingcompletedeventargs (e) håndtere data fra hvert ping der kommer tilbage.
+        {
+            string ip = (string)e.UserState; //laver en string til IP'en. UserState bliver brugt til asynkron modtagelse af svar fra MyPing.
+            if (e.Reply != null && e.Reply.Status == IPStatus.Success) //hvis ping kommer tilbage med en et svar og svaret er en succes
+            {
+                if (resolveNames) //hvis der er reply fra en host finder den navn og IP på den host
+                {
+                    string name; //laver en variable "name" til at gemme data fra den host.
+                    try
+                    {
+                        IPHostEntry hostEntry = Dns.GetHostEntry(ip); //tilføjer IP'en fra variablen ip. (reply som string)
+                        name = hostEntry.HostName; //gemmer computernavnet + netværksnavn i variablen name
+                    }
+                    catch (SocketException) //hvis der er svar, men den ikke kan få navn på host
+                    {
+                        name = " Skjult af Firewall"; //for host name "skjult af firewall"
+                    }
+                    txtHosts.AppendText(ip + " " + name + "\r\n"); //skriver ud til vinduet 
+                }
+                lock (Obj)
+                {
+                    upCount++; //giver et signal til countdown om at der er fundet en host
+                }
+            }
+            countdown.Signal(); // snakker sammen med den anden countdown.signal om hvor mange der er fundet
+        }
+
+        //Loop der pinger efter andre enheder i subnet
+        public void scan(string subnet)
+        {
+            countdown = new CountdownEvent(1); //tæller hvor mange host der bliver fundet
+            string ipBase = txtIP.Text; //variable ipBase som bliver brugt til input til scanner
+            for (int i = 1; i < 255; i++) // for loop som scanner fra 1 til 255
+            {
+                string ip = ipBase + i.ToString(); //variable ip som tager input og tilføjere 1-255 fra loopet
+
+                Ping myPing = new Ping(); // vi pinger hvert tal fra 1-255
+                myPing.PingCompleted += new PingCompletedEventHandler(P_PingCompleted); //kører igennem funktionen i bunden.
+                countdown.AddCount(); //tilføjer 1 til tæller
+                myPing.SendAsync(ip, 10000, ip); //sender asynkront ud til de 255 ip'er der er. venter i op til 10000ms på svar. 
+            }
+            countdown.Signal(); // snakker sammen med den anden countdown.signal om hvor mange der er fundet
+            countdown.Wait(); //venter med at sende at give det endelige resultat til upCount til der er svar fra alle signaler i loopet.
+
+            txtHosts.AppendText(upCount + " Fundet total"); //skriver hvor mange host der er fundet.
+        }
+        //Scan knappen
+        private void cmdScan_Click(object sender, EventArgs e) //start knap til subnet scanner
+        {           
+            myThread = new Thread(() => scan(txtIP.Text));
+            myThread.Start();
+
+            IPHostEntry host;
+            string localIP;
+
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetworkV6")
+                {
+                    localIP = ip.ToString();
+                }
+            }
+
+            if (myThread.IsAlive) //når der bliver trykket scan
+            {
+                cmdReset.Enabled = true; //reset knappen er synlig
+                cmdScan.Enabled = false; //man kan ikke trykke på scan
+                txtIP.Enabled = false; //ip add. vinduet er kan man ikke skrive i
+            }
+            else //sætte dem tilbage så det er muligt at skrive en ip og trykke på scan
+            {              
+                cmdScan.Enabled = true; 
+                txtIP.Enabled = true;
+            }
+        }
+
+        private void cmdReset_Click(object sender, EventArgs e) // clear knap til subnet scanner, som nulstiller  (reset knappen)
+        {
+            cmdScan.Enabled = true;
+            cmdReset.Enabled = false;
+            txtIP.Enabled = true;
+            upCount = 0;
+            txtHosts.Clear();
+            txtIP.Clear();
+            txtIP.Focus();
+        }
+    }
+}
